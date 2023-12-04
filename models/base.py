d2f6130510d97b9a286db8bce68c5ce5f31f3ece
gh0st <maratka2202@gmail.com>
2023-12-04 21:37:50 +0300

    Security fixes......

--- models/base.py
+++ models/base.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
 
-engine = create_async_engine("postgresql+asyncpg://postgres:root@localhost/aiohttptestserver")
+from settings import DB_PASS, DB_HOST, DB_NAME, DB_USERNAME
+
+engine = create_async_engine(f"postgresql+asyncpg://{DB_USERNAME}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
 
 Base = declarative_base()
